d86209ac8e6f69c39a26515d542d7be30e3e0758
Tobias Wich <tobias.wich@ecsec.de>
2020-11-04 08:41:37 +0100

    Add GET user call

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::net::{SocketAddr, ToSocketAddrs};
-use sqlx::sqlite::{SqliteRow, SqlitePool};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::SqliteQueryAs;
+use sqlx::sqlite::{SqlitePool, SqliteRow};
 use sqlx::{Pool, Row};
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::env;
+use std::net::{SocketAddr, ToSocketAddrs};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let db = match open_db().await {
+    let db_file = env_or("DB_FILE", "/tmp/strichliste.sqlite");
+    let db = match open_db(db_file.as_str()).await {
         Ok(db) => db,
         Err(e) => panic!("{}", e),
     };
@@ -14,20 +20,27 @@ async fn main() {
         Err(e) => panic!("{}", e),
     };
 
-    // TODO: add error handling and read addr from env var
-    let mut addr_iter = "[::]:3030".to_socket_addrs().unwrap();
+    // TODO: add error handling
+    let addr_str = env_or("BIND_ADDRESS", "[::]:3030");
+    let mut addr_iter = addr_str.to_socket_addrs().unwrap();
     let addr = addr_iter.next().unwrap();
 
-    start_webserver(addr).await;
+    start_webserver(addr, db).await;
 }
 
+fn env_or(key: &str, default: &str) -> String {
+    return env::var(key).ok().unwrap_or(default.to_string());
+}
 
-async fn open_db() -> Result<SqlitePool, sqlx::Error> {
-    let db = Pool::new("sqlite:/tmp/strichliste.sqlite");
+async fn open_db(db_file: &str) -> Result<SqlitePool, sqlx::Error> {
+    let db_string = format!("sqlite:{}", db_file);
+    let db = Pool::new(db_string.as_str());
     return db.await;
 }
 
 async fn migrate_db(db: &SqlitePool) -> Result<(), sqlx::Error> {
+    println!("Checking DB migration ...");
+
     // enable WAL mode
     //db.execute("PRAGMA journal_mode=WAL;")?;
 
@@ -46,6 +59,7 @@ async fn migrate_db(db: &SqlitePool) -> Result<(), sqlx::Error> {
         .await?;
 
     if cur_version == 0 {
+        println!("Running migration #{}", cur_version + 1);
         sqlx::query("BEGIN TRANSACTION;
 
                     CREATE TABLE article (
@@ -95,22 +109,106 @@ async fn migrate_db(db: &SqlitePool) -> Result<(), sqlx::Error> {
                     CREATE INDEX idx_transaction_articleid ON transactions (article_id);
 
                     INSERT INTO version VALUES(1);
-                
+
                     END TRANSACTION;")
                     .execute(db).await?;
     }
 
-    // db.execute("CASE WHEN SELECT num FROM version WHERE num = 1 THEN
-    //  CREATE TABLE version (num ")?;
-
     return Ok(());
 }
 
-async fn start_webserver(addr: SocketAddr) {
+#[derive(Debug, sqlx::FromRow)]
+struct UserEntity {
+    id: i32,
+    name: String,
+    email: Option<String>,
+    balance: i32,
+    disabled: bool,
+    created: String,
+    updated: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, sqlx::FromRow)]
+struct UserResponse {
+    id: i32,
+    name: String,
+    email: Option<String>,
+    balance: i32,
+    #[serde(rename(serialize = "isActive", deserialize = "isActive"))]
+    active: bool,
+    #[serde(rename(serialize = "isDisabled", deserialize = "isDisabled"))]
+    disabled: bool,
+    created: String,
+    updated: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct UsersResponse {
+    users: Vec<UserResponse>,
+}
+
+async fn start_webserver(addr: SocketAddr, db: SqlitePool) {
+    println!("Starting webserver binding to {} ...", addr);
+
     // see next link how to add apis
     // https://blog.logrocket.com/creating-a-rest-api-in-rust-with-warp/
 
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let users = warp::get()
+        .and(warp::path!("user"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and(with_db(db))
+        .and_then(get_users);
 
-    warp::serve(hello).run(addr).await;
+    let api = warp::path("api").and(hello.or(users));
+
+    warp::serve(api).run(addr).await;
+}
+
+fn with_db(
+    db_pool: SqlitePool,
+) -> impl Filter<Extract = (SqlitePool,), Error = Infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
+}
+
+async fn get_users(
+    query: HashMap<String, String>,
+    db: SqlitePool,
+) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let deleted: bool = query
+        .get("deleted")
+        .map(|v| v.parse().ok())
+        .flatten()
+        .unwrap_or(false);
+
+    let user_entities_result = sqlx::query_as::<_, UserEntity>("SELECT id, name, email, balance, disabled, created, updated FROM user WHERE disabled IS ?")
+        .bind(deleted.to_string())
+        .fetch_all(&db);
+
+    let result: UsersResponse;
+    match user_entities_result.await {
+        Ok(user_entities) => {
+            let user_responses = user_entities
+                .iter()
+                .map(|v| UserResponse {
+                    id: v.id,
+                    name: v.name.clone(),
+                    email: v.email.clone(),
+                    balance: v.balance,
+                    active: true,
+                    disabled: v.disabled,
+                    created: v.created.clone(),
+                    updated: v.updated.clone(),
+                })
+                .collect();
+            result = UsersResponse {
+                users: user_responses,
+            };
+            return Ok(Box::new(warp::reply::json(&result)));
+        }
+        Err(e) => {
+            println!("Failed to query user table. {}", e);
+            return Ok(Box::new(warp::http::StatusCode::INTERNAL_SERVER_ERROR));
+        }
+    };
 }
