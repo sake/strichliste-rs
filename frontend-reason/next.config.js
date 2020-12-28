cfddb915802b2b619f4972726e40dcda6d550346
Tobias Wich <tobias.wich@electrologic.org>
2020-12-28 12:57:46 +0100

    Remove transpile-modules

--- frontend-reason/next.config.js
+++ frontend-reason/next.config.js
@@ -1,5 +1,5 @@
-const withTM = require('next-transpile-modules')(['bs-platform'])
-
-module.exports = withTM({
-  pageExtensions: ['jsx', 'js', 'bs.js'],
-})
+module.exports = {
+    pageExtensions: ['jsx', 'js', 'bs.js'],
+    reactStrictMode: true,
+    target: 'serverless',
+}
